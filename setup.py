ee947cb257d0c823834c3b95bf3c311987870eaa
Simon Litchfield <simon@s29.com.au>
2014-05-12 22:26:17 +1000

    Pipped

---
+++ setup.py
@@ -0,0 +1,38 @@
+from distutils.core import setup
+from distutils.command.install import INSTALL_SCHEMES
+import os
+
+root = os.path.dirname(os.path.abspath(__file__))
+os.chdir(root)
+
+VERSION = '0.1'
+
+# Make data go to the right place.
+# http://groups.google.com/group/comp.lang.python/browse_thread/thread/35ec7b2fed36eaec/2105ee4d9e8042cb
+for scheme in INSTALL_SCHEMES.values():
+    scheme['data'] = scheme['purelib']
+
+
+setup(
+    name='django-fastsitemaps',
+    version=VERSION,
+    description="Fast, streaming sitemaps for Django",
+    long_description="Drop-in replacement for django.contrib.sitemaps that gives you fast, streaming sitemaps that consume minimal memory (O^1 instead of O^n) and minimal server response time on huge data sets.",
+    author="Simon Litchfield",
+    author_email="simon@s29.com.au",
+    url="http://github.com/litchfield/django-fastsitemaps",
+    license="MIT License",
+    platforms=["any"],
+    packages=['fastsitemaps'],
+    #data_files=[(template_dir, templates)],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Framework :: Django",
+    ],
+    include_package_data=True,
+)
